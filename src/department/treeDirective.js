b01484f5277d128405e40add8ebd736dc7275489
Nadia M <leavingofnight@gmail.com>
2018-05-24 08:57:57 +0500

    lists data flow

--- src/department/treeDirective.js
+++
@@ -1,9 +0,0 @@
-app.directive('tree', function () {
-   return {
-       restrict: 'E',
-       scope: {
-           collection: '='
-       },
-       template: '<ul><node ng-repeat="member in collection | filter: data.search.name | orderBy: '+'\''+'name'+'\''+'" member="member"></node></ul>'
-   }
-});
